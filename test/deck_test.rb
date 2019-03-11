f39c399195982135fb026d74e4eec9b2a0670bfb
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-11 17:54:19 -0600

    Adds Deck class and tests

---
+++ test/deck_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+class DeckTest < Minitest::Test
+  def test_deck_has_3_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_instance_of Array, deck.cards
+    assert_equal 3, deck.count
+  end
+
+  def test_deck_sorts_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal 2, deck.cards_in_category(:STEM).count
+    assert_equal 1, deck.cards_in_category(:Geography).count
+    assert_equal 0, deck.cards_in_category("Pop Culture").count
+  end
+
+end
