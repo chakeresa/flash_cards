5388616bb899a3f57b39c2b65385d30a55cba658
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-16 21:45:08 -0600

    Add TurnTest assertion for guess not being case sensitive

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -33,9 +33,16 @@ class TurnTest < Minitest::Test
     assert_nil turn.card
   end
 
-  def test_right_answer_is_correct # to do: add a test that it's not case sensitive
+  def test_right_answer_is_correct
     card = Card.new("What is the abbreviation for iron?", "Fe", :STEM)
     turn = Turn.new("Fe", card)
+
+    assert turn.correct?
+  end
+
+  def test_right_answer_is_not_case_sensitive
+    card = Card.new("What is the abbreviation for iron?", "Fe", :STEM)
+    turn = Turn.new("FE", card)
 
     assert turn.correct?
   end
@@ -61,6 +68,7 @@ class TurnTest < Minitest::Test
     assert_equal "Incorrect.", turn.feedback
   end
 
+# delete me - not relevant for TurnTest
   def test_card_answer_returns_answer
     card = Card.new("What is the abbreviation for iron?", "Fe", :STEM)
     turn = Turn.new("Ir", card)
